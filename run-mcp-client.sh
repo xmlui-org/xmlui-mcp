595d71580d608a56931e4cbcb49e6f05da385b0c
Jon Udell <jon@jonudell.info>
2025-05-11 12:28:38 -0700

    update run-mcp-client.sh

--- run-mcp-client.sh
+++ run-mcp-client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for example...
-./xmlui-mcp-client "/Users/jonudell/xmlui-getting-started/mcp/xmlui-mcp /Users/jonudell/xmlui-getting-started /Users/jonudell xmlui-hn,xmlui-mastodon"
+./xmlui-mcp-client "/Users/jonudell/xmlui-getting-started/mcp/xmlui-mcp /Users/jonudell/xmlui-getting-started /Users/jonudell xmlui-hn,xmlui-mastodon,xmlui-cms"
 
 
 
